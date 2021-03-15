fb654780596f968d9734baa881683d9ad93be667
manos-fr <manoskakarakis@gmail.com>
2021-03-15 18:11:22 +0200

    fixes connection with database and gets rid of client code

--- python_server.py
+++ python_server.py
@@ -9,30 +9,27 @@ import requests
 app = Flask(__name__)
 api = Api(app)
 
+
 @app.route('/song_name')
 def get_tab():
     name = request.args.get('name')
     try:
-        # Connect to an existing database
-        connection = psycopg2.connect(user="manosdb", password="12345", 
-        host="127.0.0.1", port="5432", database="manosdb")
         # Create a cursor to perform database operations
         cursor = connection.cursor()
-        # Executing a SQL query
-        cursor.execute("SELECT version();")
-
         with connection:
-            cursor.execute(f"SELECT tab FROM songs WHERE name = '{name}'")  
-            response = cursor.fetchall()  
+            cursor.execute(f"SELECT tab FROM songs WHERE name = '{name}'")
+            response = cursor.fetchall()
             resp = ''.join(map(str, response))
+            tabsStr = ''.join(map(str, resp))
+            formatted_output = tabsStr.replace(
+                '\\n', '\n').replace('\\t', '\t')
     except (Exception, Error) as error:
         print("Error while connecting to PostgreSQL", error)
 
-    finally:
-        if (connection):
-            cursor.close()
-            connection.close()
-    return json.dumps(resp)
+    return formatted_output
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=3001,debug=True,threaded=True)
+    connection = psycopg2.connect(user="manosdb", password="12345",
+                                  host="127.0.0.1", port="5432", database="manosdb")
+    app.run(host="0.0.0.0", port=3001, debug=True, threaded=True)
